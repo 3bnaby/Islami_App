547300ba44daa91df4e5028454ae79aa22bedf1f
3bnaby <mahmoud.mohamad68@gmail.com>
2024-08-20 02:35:55 +0300

    create quraan fragment layout

--- app/src/main/java/com/example/islami_app/ui/screens/home/HomeActivity.kt
+++ app/src/main/java/com/example/islami_app/ui/screens/home/HomeActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.islami_app.R
 import com.example.islami_app.databinding.ActivityHomeBinding
+import com.example.islami_app.ui.screens.home.tabs.ahadeth.AhadethFragment
+import com.example.islami_app.ui.screens.home.tabs.quraan.QuraanFragment
+import com.example.islami_app.ui.screens.home.tabs.radio.RadioFragment
+import com.example.islami_app.ui.screens.home.tabs.sebha.SebhaFragment
 
 class HomeActivity : AppCompatActivity() {
 
@@ -17,5 +22,39 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        showFragment(QuraanFragment())
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.bottomNavView.setOnItemSelectedListener {menuItem ->
+            when(menuItem.itemId){
+                R.id.quraan_item -> {
+                    showFragment(QuraanFragment())
+                }
+                R.id.sebha_item -> {
+                    showFragment(SebhaFragment())
+
+                }
+                R.id.radio_item -> {
+                    showFragment(RadioFragment())
+
+                }
+                R.id.ahadeth_item -> {
+                    showFragment(AhadethFragment())
+
+                }
+                else -> {
+
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun showFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container , fragment)
+            .commit()
     }
 }
