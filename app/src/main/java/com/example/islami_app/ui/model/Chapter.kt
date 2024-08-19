4f752837ef1f5b3224ff4a229dd21a35f56deae5
3bnaby <mahmoud.mohamad68@gmail.com>
2024-08-20 02:59:17 +0300

    finished quraan fragment with data of chapters and it's verses

---
+++ app/src/main/java/com/example/islami_app/ui/model/Chapter.kt
@@ -0,0 +1,3 @@
+package com.example.islami_app.ui.model
+
+data class Chapter(val name: String, val versesNumber: Int)
---
+++ app/src/main/java/com/example/islami_app/ui/screens/home/tabs/quraan/ChaptersAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.islami_app.ui.screens.home.tabs.quraan
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.islami_app.databinding.ItemChapterBinding
+import com.example.islami_app.ui.model.Chapter
+
+class ChaptersAdapter(val chapters : List<Chapter>) :
+    Adapter<ChaptersAdapter.ChaptersViewHolder>() {
+
+
+
+    class ChaptersViewHolder(val binding:ItemChapterBinding) : ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChaptersViewHolder {
+        val binding = ItemChapterBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
+        return ChaptersViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = chapters.size
+
+    override fun onBindViewHolder(holder: ChaptersViewHolder, position: Int) {
+        val chapter = chapters[position]
+        holder.binding.chapterNameTv.text = chapter.name
+        holder.binding.chapterVersesTv.text= chapter.versesNumber.toString()
+    }
+}
